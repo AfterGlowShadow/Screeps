ccaabb8181e8460ccb1ac11bac69168c4ea7ec17
iPlanC <planc2333@outlook.com>
2020-03-09 17:50:55 +0800

    Update role.miner.js

--- role.miner.js
+++ role.miner.js
@@ -2,7 +2,7 @@ var roleMiner = {
     
     run: function(creep) {
         
-        var storage = creep.room.storage;
+        var terminal = creep.room.terminal;
         
         if (creep.store.getFreeCapacity() > 0) {
             var mineral = creep.pos.findClosestByRange(FIND_MINERALS);
@@ -15,15 +15,15 @@ var roleMiner = {
                 filter: (structure) =>  (structure.structureType == STRUCTURE_LAB)
             });
             if (labs.length > 0) {
-                if (lans[3].store.getFreeCapacity == 0) {
+                if (labs[3].store.getFreeCapacity == 0) {
                     if (creep.transfer(labs[3], RESOURCE_UTRIUM) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(labs[3], {visualizePathStyle: {stroke: '#ffffff'}});
                     }
                 }
-                else {
-                    if (creep.transfer(storage, RESOURCE_UTRIUM) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(storage, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
+            }
+            else {
+                if (creep.transfer(terminal, RESOURCE_UTRIUM) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(terminal, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
