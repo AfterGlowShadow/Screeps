56677bf2c879a89db65ad2123647fd3bfc0ed368
iPlanC <planc2333@outlook.com>
2020-03-10 22:09:47 +0800

    Update auto.export.console.js

--- auto.export.console.js
+++ auto.export.console.js
@@ -4,22 +4,22 @@ var consoleExport = {
         console.log();
         console.log();
         
-        console.log('Game Time is: ' + Game.time + '.');
+        console.log('Game Time is: <font color="red">' + Game.time + '</font>.');
         
-        console.log('You have ' + Game.market.credits + ' Credits aviliable.');
+        console.log('You have <font color="red">' + Game.market.credits + '</font> Credits aviliable.');
 
         for (var name in Game.rooms) {
             if (Game.rooms[name].controller == null) return;
-            console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + '/' + Game.rooms[name].energyCapacityAvailable + ' energy.\t' +
-                        'Room "' + name + '" controller level is ' + Game.rooms[name].controller.level + ' , progress left ' + (Game.rooms[name].controller.progressTotal - Game.rooms[name].controller.progress) + '.\t');
+            console.log('Room "<font color="red">' + name + '</font>" has <font color="red">' + Game.rooms[name].energyAvailable + '</font>/<font color="red">' + Game.rooms[name].energyCapacityAvailable + '</font> energy.\t' +
+                        'Room "<font color="red">' + name + '</font>" controller level is <font color="red">' + Game.rooms[name].controller.level + '</font> , progress left <font color="red">' + (Game.rooms[name].controller.progressTotal - Game.rooms[name].controller.progress) + '</font>.\t');
         }
         
-        console.log('Room "' + 'W34N2' + '" has ' + Game.rooms['W34N2'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + ' Energy to sell, market mode is ' + marketMode + '.');
+        console.log('Room "' + 'W34N2' + '" has <font color="red">' + Game.rooms['W34N2'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + '</font> Energy to sell, market mode is <font color="red">' + marketMode + '</font>.');
         
         for (var name in Memory.creeps) {
             if (!Game.creeps[name]) {
                 delete Memory.creeps[name];
-                console.log('Clearing non-existing creep memory:', name);
+                console.log('Clearing non-existing creep memory:<font color="red">' +  name + '</font>.');
             }
         }
     }
