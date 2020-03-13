8c434bb19e01a1329f43f30de9d65a0f45857a9f
iPlanC <planc2333@outlook.com>
2020-03-13 08:38:44 +0800

    update

--- auto.export.console.js
+++ auto.export.console.js
@@ -6,27 +6,27 @@ var consoleExport = {
     run: function(marketMode) {
         console.log();
         console.log();
-        
+
         newTime = new Date().getTime();
-        console.log('Game Time is: <font color="yellow">' + Game.time + '</font> ' + '(<font color="yellow">' + ((newTime - oldTime) / 1000) + '</font>second).');
+        console.log('Game Time is: <font color="cyan">' + Game.time + '</font> ' + '(<font color="cyan">' + ((newTime - oldTime) / 1000) + '</font>second).');
         oldTime = newTime;
-        
-        console.log('You have <font color="yellow">' + Game.market.credits + '</font> Credits aviliable.');
+
+        console.log('You have <font color="cyan">' + Game.market.credits + '</font> Credits aviliable.');
 
         for (var name in Game.rooms) {
             if (Game.rooms[name].controller == null) return;
-            console.log('Room "<font color="yellow">' + name + '</font>' + 
-                        '" has <font color="yellow">' + Game.rooms[name].energyAvailable + '</font>/<font color="yellow">' + Game.rooms[name].energyCapacityAvailable + '</font> energy' +
-                        ', controller level is <font color="yellow">' + Game.rooms[name].controller.level + '</font>' + 
-                        ', progress left <font color="yellow">' + (Game.rooms[name].controller.progressTotal - Game.rooms[name].controller.progress) + '</font>.\t');
+            console.log('Room "<font color="cyan">' + name + '</font>' +
+                        '" has <font color="cyan">' + Game.rooms[name].energyAvailable + '</font>/<font color="cyan">' + Game.rooms[name].energyCapacityAvailable + '</font> energy' +
+                        ', controller level is <font color="cyan">' + Game.rooms[name].controller.level + '</font>' +
+                        ', progress left <font color="cyan">' + (Game.rooms[name].controller.progressTotal - Game.rooms[name].controller.progress) + '</font>.\t');
         }
 
-        console.log('Room "' + 'W34N2' + '" has <font color="yellow">' + Game.rooms['W34N2'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + '</font> Energy to sell, market mode is <font color="yellow">' + marketMode + '</font>.');
+        console.log('Room "' + 'W34N2' + '" has <font color="cyan">' + Game.rooms['W34N2'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + '</font> Energy to sell, market mode is <font color="cyan">' + marketMode + '</font>.');
 
         for (var name in Memory.creeps) {
             if (!Game.creeps[name]) {
                 delete Memory.creeps[name];
-                console.log('Clearing non-existing creep memory:<font color="yellow">' +  name + '</font>.');
+                console.log('Clearing non-existing creep memory:<font color="cyan">' +  name + '</font>.');
             }
         }
     }
--- auto.market.js
+++ auto.market.js
@@ -5,18 +5,18 @@ var autoDeal = {
             for (var name in Game.rooms) {
                 if (Game.rooms[name].terminal == null) continue;
                 Terminal = Game.rooms[name].terminal;
-                if (Terminal.store.getUsedCapacity(RESOURCE_UTRIUM) == 0) {
+                if (Terminal.store.getUsedCapacity(RESOURCE_UTRIUM) < 1000) {
                     var buyOrder = Game.market.getAllOrders({type: ORDER_BUY, resourceType: RESOURCE_ENERGY});
                     var tax = Game.market.calcTransactionCost(1000, buyOrder[0].roomName, Terminal.pos.roomName);
                     if (buyOrder[0].price > 0.010) {
-                        Game.market.deal(buyOrder[0].id, 500, Terminal.pos.roomName);
+                        Game.market.deal(buyOrder[0].id, 1000, Terminal.pos.roomName);
                     }
                 }
                 else {
                     var buyOrder = Game.market.getAllOrders({type: ORDER_BUY, resourceType: RESOURCE_UTRIUM});
                     var tax = Game.market.calcTransactionCost(1000, buyOrder[0].roomName, Terminal.pos.roomName);
-                    if (buyOrder[0].price > 0.010) { 
-                        Game.market.deal(buyOrder[0].id, 500, Terminal.pos.roomName);
+                    if (buyOrder[0].price > 0.010) {
+                        Game.market.deal(buyOrder[0].id, 1000, Terminal.pos.roomName);
                     }
                 }
             }
